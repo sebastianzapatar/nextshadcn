4c4351b42c3d592b88393082a0becacd02190c82
Sebastian Zapata <pansezapata@gmail.com>
2025-05-14 21:15:39 -0500

    Se organiza para desplegar

--- app/(general)/chef/[id]/page.tsx
+++ app/(general)/chef/[id]/page.tsx
@@ -1,8 +1,11 @@
+// app/(general)/chef/[id]/page.tsx
+
 import ChefDetail from "@/app/components/chef/ChefDetail";
-interface Props {
-    params: { id: string };
-  }
-  
-  export default async function ChefDetailPage({ params }: Props) {
-    return <ChefDetail id={params.id} />;
-  }
+
+export default async function Page(props: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await props.params; 
+
+  return <ChefDetail id={id} />;
+}
